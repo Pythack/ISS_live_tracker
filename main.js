957bfcac0b1e4a780e140b1e456e2fc0b302861f
Pythack <59161870+Pythack@users.noreply.github.com>
2020-07-10 21:33:10 +0200

    Update main.js

--- main.js
+++ main.js
@@ -105,27 +105,6 @@ xmlhttp.open("GET", url, true);
 xmlhttp.send();
 }
 
-ip_get('https://api.ipify.org/', function(data) {
-var ip = data;
-loc_get('https://ipapi.co/' + ip + '/json', function(data) {
-var latitude = data['latitude'];
-var longitude = data['longitude'];
-//document.getElementById('y_lat').innerHTML = "<p id='y_lat'>Your latitude : " + data['latitude'] + "°</p>";
-//document.getElementById('y_lon').innerHTML = "<p id='y_lat'>Your longitude : " + data['longitude'] + "°</p>";
-prev_get('https://www.n2yo.com/rest/v1/satellite/visualpasses/25544/' + data['latitude'] + '/' + data['longitude'] + '/10/10/10/&apiKey=7VFHXQ-LBZVP5-3Y4ZGN-4HWY', function(data) {
-  var response = data['passes'];
-  var prevision = response['0'];
-  var duration = prevision['duration'];
-  var timestamp = prevision['startUTC'];
-  timestamp_get('https://showcase.api.linx.twenty57.net/UnixTime/fromunix?timestamp=' + timestamp, function(data) {
-    document.getElementById('next_pass').innerHTML = "<p id='next_pass'>Next pass : " + data + "</p>";
-  });
-  document.getElementById('next_pass_duration').innerHTML = "<p id='next_pass_duration'>Next pass duration : " + duration + " seconds</p>";
-});
-});
-});
-
-
 
 function capitalizeFirstLetter(string) {
 return string.charAt(0).toUpperCase() + string.slice(1);
@@ -151,5 +130,25 @@ document.getElementById('visibility_table').innerHTML = "<p id='visibility_table
 document.getElementById('solar_lat_table').innerHTML = "<p id='solar_lat_table'>Solar latitude : " + data['solar_lat'] + "°</p>";
 document.getElementById('solar_lon_table').innerHTML = "<p id='solar_lon_table'>Solar longitude : " + data['solar_lon'] + "°</p>";
 });
+ip_get('https://api.ipify.org/', function(data) {
+var ip = data;
+loc_get('https://ipapi.co/' + ip + '/json', function(data) {
+var latitude = data['latitude'];
+var longitude = data['longitude'];
+//document.getElementById('y_lat').innerHTML = "<p id='y_lat'>Your latitude : " + data['latitude'] + "°</p>";
+//document.getElementById('y_lon').innerHTML = "<p id='y_lat'>Your longitude : " + data['longitude'] + "°</p>";
+prev_get('https://www.n2yo.com/rest/v1/satellite/visualpasses/25544/' + data['latitude'] + '/' + data['longitude'] + '/10/10/10/&apiKey=7VFHXQ-LBZVP5-3Y4ZGN-4HWY', function(data) {
+  var response = data['passes'];
+  var prevision = response['0'];
+  var duration = prevision['duration'];
+  var timestamp = prevision['startUTC'];
+  timestamp_get('https://showcase.api.linx.twenty57.net/UnixTime/fromunix?timestamp=' + timestamp, function(data) {
+    document.getElementById('next_pass').innerHTML = "<p id='next_pass'>Next pass : " + data + "</p>";
+  });
+  document.getElementById('next_pass_duration').innerHTML = "<p id='next_pass_duration'>Next pass duration : " + duration + " seconds</p>";
+});
+});
+});
+
 }
 var t=setInterval(actualize,1500);
