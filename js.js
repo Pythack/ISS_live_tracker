68f548be8df10a5670b797330969688cf57ac497
Pythack <59161870+Pythack@users.noreply.github.com>
2020-07-10 11:02:25 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-function ajax_get(url, callback) {
+function iss_get(url, callback) {
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
 if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
@@ -22,7 +22,7 @@ xmlhttp.send();
 function capitalizeFirstLetter(string) {
 return string.charAt(0).toUpperCase() + string.slice(1);
 }
-ajax_get('https://api.wheretheiss.at/v1/satellites/25544', function(data) {
+iss_get('https://api.wheretheiss.at/v1/satellites/25544', function(data) {
 document.getElementById('latitude_table').innerHTML = "<p id='latitude_table'>Latitude : " + data['latitude'] + "°</p>";
 document.getElementById('longitude_table').innerHTML = "<p id='longitude_table'>Longitude : " + data['longitude'] + "°</p>";
 document.getElementById('altitude_table').innerHTML = "<p id='altitude_table'>Altitude : " + data['altitude'] + " km</p>";
@@ -33,7 +33,7 @@ document.getElementById('solar_lat_table').innerHTML = "<p id='solar_lat_table'>
 document.getElementById('solar_lon_table').innerHTML = "<p id='solar_lon_table'>Solar longitude : " + data['solar_lon'] + "°</p>";
 });
 function actualize() {
-ajax_get('https://api.wheretheiss.at/v1/satellites/25544', function(data) {
+iss_get('https://api.wheretheiss.at/v1/satellites/25544', function(data) {
 document.getElementById('latitude_table').innerHTML = "<p id='latitude_table'>Latitude : " + data['latitude'] + "°</p>";
 document.getElementById('longitude_table').innerHTML = "<p id='longitude_table'>Longitude : " + data['longitude'] + "°</p>";
 document.getElementById('altitude_table').innerHTML = "<p id='altitude_table'>Altitude : " + data['altitude'] + " km</p>";
