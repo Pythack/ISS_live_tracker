51ebc902457307da64d49df1b29393efad944be8
Pythack <59161870+Pythack@users.noreply.github.com>
2020-07-11 16:03:20 +0200

    Update main.js

--- main.js
+++ main.js
@@ -118,14 +118,14 @@ prev_get('https://www.n2yo.com/rest/v1/satellite/visualpasses/25544/' + data['la
   var prevision = response['0'];
   var duration = prevision['duration'];
   var timestamp = prevision['startUTC'];
-  var staz = prevision['startAz'];
-  var enaz = prevision['endAz'];
+  //var staz = prevision['startAz'];
+  //var enaz = prevision['endAz'];
   timestamp_get('https://showcase.api.linx.twenty57.net/UnixTime/fromunix?timestamp=' + timestamp, function(data) {
     document.getElementById('next_pass').innerHTML = "<p id='next_pass'>Next pass : " + data + "</p>";
   });
   document.getElementById('next_pass_duration').innerHTML = "<p id='next_pass_duration'>Next pass duration : " + duration + " seconds</p>";
-  document.getElementById('start_az').innerHTML = "<p id='start_az'>Start azimuth : " + staz + "°</p>";
-  document.getElementById('end_az').innerHTML = "<p id='end_az'>End azimuth : " + enaz + "°</p>";
+  //document.getElementById('start_az').innerHTML = "<p id='start_az'>Start azimuth : " + staz + "°</p>";
+  //document.getElementById('end_az').innerHTML = "<p id='end_az'>End azimuth : " + enaz + "°</p>";
   });
 });
 });
