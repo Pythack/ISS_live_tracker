0ab919a8f0acb856c76b5320545ecd671028f374
Pythack <59161870+Pythack@users.noreply.github.com>
2020-07-18 14:55:45 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -135,10 +135,7 @@ if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
         return;
     }
     callback(astro);
-} else {
-  console.warn('Please desactivate securisation of the page to access astronautes infos. ');
 }
-
 };
 
 xmlhttp.open("GET", url, true);
